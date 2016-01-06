8750755b9d9cf8e2bcbe16359e1d63103accaa7a
Will Field <willjfield@gmail.com>
2016-01-05 19:10:16 -0500

    random landscape

--- threeLandscape/script.js
+++ threeLandscape/script.js
@@ -1,22 +1,28 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
+var controls = new THREE.OrbitControls( camera );
+                                controls.damping = 0.2;
+			
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var geometry = new THREE.PlaneGeometry( 50, 50, 50,50 );
+var material = new THREE.MeshBasicMaterial( { color: 0xdddddd, wireframe: true } );
 
-camera.position.z = 5;
+for(var v=0;v<geometry.vertices.length;v++){
+	geometry.vertices[v].z = Math.random()*2
+}
+var landscape = new THREE.Mesh( geometry, material );
+
+scene.add( landscape );
+
+camera.position.z = 50;
 
 var render = function () {
-	requestAnimationFrame( render );
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+	requestAnimationFrame( render );
+	landscape.rotation.x = -45
 
 	renderer.render(scene, camera);
 };
