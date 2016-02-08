4ec83a18c851ba76f78556fff3062a6812b920ca
Will Field <willjfield@gmail.com>
2016-02-08 11:51:16 -0500

    rotations, wireframes

--- solarSystem/_script.js
+++ solarSystem/_script.js
@@ -5,12 +5,12 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-				controls.enableDamping = true;
-				controls.dampingFactor = 0.25;
+var controls = new THREE.OrbitControls(camera);
+				//controls.enableDamping = true;
+				//controls.dampingFactor = 0.25;
 				controls.minDistance = 0;
-				controls.minPolarAngle = 0; // radians
-				controls.maxPolarAngle = Math.PI*2
+				/*controls.minPolarAngle = 0; // radians
+				controls.maxPolarAngle = Infinity;*/
 
 var light = new THREE.PointLight( 0xffffff, 1, 0 );
 light.position.set( 0, 0, 0 );
@@ -22,7 +22,7 @@ scene.add( light );
 var t = 0;
 var drawPlanets = []
 var solScale = 30;
-var planScale = 2000;
+var planScale = 5000;
 
 //SUN
 var geometry = new THREE.SphereGeometry( 5,16,16 );
@@ -33,8 +33,13 @@ scene.add( sphere );
 //PLANETS
 for(var p in explore.planets){
 	var geometry = new THREE.SphereGeometry( explore.planets[p].radius/planScale,8,8 );
-	var material = new THREE.MeshLambertMaterial( { color: explore.planets[p].texColor, wireframe: true } );
+	var material = new THREE.MeshLambertMaterial( { color: explore.planets[p].texColor } );
 	var sphere = new THREE.Mesh( geometry, material );
+
+	var materialW = new THREE.MeshLambertMaterial( { color: 0x000000, wireframe: true, wireframeLinewidth: .5 } );
+	var sphereW = new THREE.Mesh( geometry, materialW );
+	sphere.add(sphereW);
+	
 	var curPosition = explore.SolarSystem(explore.planets[p],explore.now);
 	sphere.position.set(curPosition[0]*solScale,curPosition[1]*solScale,curPosition[2]*solScale);
 	drawPlanets.push(sphere);
@@ -44,13 +49,15 @@ for(var p in explore.planets){
 var render = function () {
 	requestAnimationFrame( render );
 	controls.update();
-	var step = .01;
+	var step = .001;
 	t+=step;
+	console.log(t)
 	for(p in drawPlanets){
 		var curPosition = explore.SolarSystem(explore.planets[p],explore.now+t);
-		var deltaRotation = (explore.planets[p].dayLength*360)*(Math.PI/180)
-		var curRotation = explore.planets[p].dayLength+(deltaRotation*t)
-		drawPlanets[p].rotation.set(0,0,curRotation)
+		var deltaRotation = (24/explore.planets[p].dayLength)*(2*Math.PI)
+
+		var curRotation = (deltaRotation*t)
+		drawPlanets[p].rotation.set(explore.planets[p].oblique*(Math.PI/180),0,curRotation)
 		drawPlanets[p].position.set(curPosition[0]*solScale,curPosition[1]*solScale,curPosition[2]*solScale)
 	}
 	renderer.render(scene, camera);
