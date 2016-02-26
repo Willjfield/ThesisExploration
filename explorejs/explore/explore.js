452407d946c07efb48a81d0532818821a04beb2a
Will Field <willjfield@gmail.com>
2016-02-26 17:53:23 -0500

    added to orrery

--- explorejs/explore/explore.js
+++ explorejs/explore/explore.js
@@ -36,6 +36,65 @@
 	          );
 	}
 
+	explore.dateFromJday = function(jday){
+		return dateFromJday(jday)
+	}
+
+	//From http://www.onlineconversion.com/julian_date.htm
+	function dateFromJday(jd){
+			var	j1, j2, j3, j4, j5;			//scratch
+
+			//
+			// get the date from the Julian day number
+			//
+		    var intgr   = Math.floor(jd);
+		    var frac    = jd - intgr;
+		    var gregjd  = 2299161;
+			if( intgr >= gregjd ) {				//Gregorian calendar correction
+				var tmp = Math.floor( ( (intgr - 1867216) - 0.25 ) / 36524.25 );
+				j1 = intgr + 1 + tmp - Math.floor(0.25*tmp);
+			} else
+				j1 = intgr;
+
+			//correction for half day offset
+			var dayfrac = frac + 0.5;
+			if( dayfrac >= 1.0 ) {
+				dayfrac -= 1.0;
+				++j1;
+			}
+
+			j2 = j1 + 1524;
+			j3 = Math.floor( 6680.0 + ( (j2 - 2439870) - 122.1 )/365.25 );
+			j4 = Math.floor(j3*365.25);
+			j5 = Math.floor( (j2 - j4)/30.6001 );
+
+			var d = Math.floor(j2 - j4 - Math.floor(j5*30.6001));
+			var m = Math.floor(j5 - 1);
+			if( m > 12 ) m -= 12;
+			var y = Math.floor(j3 - 4715);
+			if( m > 2 )   --y;
+			if( y <= 0 )  --y;
+
+			//
+			// get time of day from day fraction
+			//
+			var hr  = Math.floor(dayfrac * 24.0);
+			var mn  = Math.floor((dayfrac*24.0 - hr)*60.0);
+			var f  = ((dayfrac*24.0 - hr)*60.0 - mn)*60.0;
+			var sc  = Math.floor(f);
+				 f -= sc;
+		    if( f > 0.5 ) ++sc;
+
+		    return {
+		    	year: y,
+		    	month: m,
+		    	day: d,
+		    	hour: hr,
+		    	minute: mn,
+		    	sec: sc
+		    }
+		}
+
 	var now = new Date(); //set any date
 
 	explore.now = jday(now.getUTCFullYear(), 
--- explorejs/trouvelot/orrery.js
+++ explorejs/trouvelot/orrery.js
@@ -5,8 +5,13 @@ var sun
 var centerX, centerY
 var initX = 0
 var initY = 0
-
+var speed = 0
+var tOffset = 0
+var date
 function preload(){
+	document.getElementById("speedSlider").min = -5
+	document.getElementById("speedSlider").max = 5
+	document.getElementById("speedSlider").step = .01
 	spaceBackground = loadImage("fludd_infinity2.jpg")
 	spaceMatte = loadImage("infinity.png")
 	sun = loadImage("img/sun.png")
@@ -17,6 +22,7 @@ function preload(){
 	planets[4] = loadImage("img/jupiter.png")
 	planets[5] = loadImage("img/saturn.png")
 }
+
 function setup(){
 	createCanvas(windowHeight, windowHeight)
 	imageMode(CENTER)
@@ -27,9 +33,23 @@ function setup(){
 	centerX = width/2
 	centerY = height/2
 }
+
 function draw(){
 	background(0)
 	drawPlanets()
+	speed = parseFloat(document.getElementById("speedSlider").value)
+	if(speed>0){
+		tOffset+=speed*speed*speed
+	}else{
+		tOffset+=speed*speed*speed
+	}
+	date = explore.dateFromJday(explore.now+tOffset)
+	noStroke()
+	fill(255)
+	textSize(30)
+	text(date.month,50,70)
+	text(date.day,100,70)
+	text(date.year,150,70)
 }
 
 function mouseWheel(event) {
@@ -53,7 +73,7 @@ function drawPlanets(){
 		pop()
 		for(var p in planets){
 			push()
-				var curPosition = explore.SolarSystem(explore.planets[p],explore.now)
+				var curPosition = explore.SolarSystem(explore.planets[p],explore.now+tOffset)
 				var distance = Math.sqrt(((curPosition[0]*25)*(curPosition[0]*25))+((curPosition[1]*25)*(curPosition[1]*25)))
 				ellipse(0,0,distance*2,distance*2)
 				translate(-curPosition[0]*25,curPosition[1]*25)
@@ -78,11 +98,17 @@ function drawPlanets(){
 }
 
 function mousePressed(){
+	if(mouseX<0||mouseX>width||mouseY<0||mouseY>height){return 0}
 	initX = mouseX-centerX
 	initY = mouseY-centerY
 }
-function mouseDragged() {
 
+function mouseReleased(){
+	document.getElementById("speedSlider").value = 0
+}
+
+function mouseDragged() {
+	if(mouseX<0||mouseX>width||mouseY<0||mouseY>height){return 0}
 	centerX = mouseX-initX
 	centerY = mouseY-initY
 
